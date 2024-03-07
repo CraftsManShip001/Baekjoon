680b179a996098271c01791dec91d454efa1b0b0
Suh-Jeong-Hyeon <150584831+CraftsManShip001@users.noreply.github.com>
2024-03-07 20:06:53 +0900

    [Bronze III] Title: 최소, 최대, Time: 192 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n,num,max = -1000001,min = 1000001;
+    scanf("%d", &n);
+    for(int i = 0;i<n;i++){
+        scanf("%d", &num);
+        if(num > max){
+            max = num;
+        }
+        if(num < min){
+            min = num;
+        }
+    }
+    printf("%d %d", min,max);
+    return 0;
+}
