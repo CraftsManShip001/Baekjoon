8de329a8b158e96135189439cb3d71160b740e8e
Suh-Jeong-Hyeon <150584831+CraftsManShip001@users.noreply.github.com>
2024-03-20 20:28:40 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.c
@@ -0,0 +1,31 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int n,m,a,b;
+    scanf("%d %d", &n, &m);
+    int arr[n+1];
+    for(int i = 1;i<=n;i++){
+        arr[i] = i;
+    }
+    for(int i = 0;i<m;i++){
+        int temp = 0;
+        scanf("%d %d", &a, &b);
+        int s = b - a;
+        for(int j = 0;j<(s + 1) / 2;j++){
+            temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+            a += 1;
+            b -= 1;
+            //printf("%d %d\n", arr[a],arr[b]);
+        }
+        
+        //printf("\n\n");
+    }
+    for(int i = 1;i<=n;i++){
+            printf("%d ", arr[i]);
+        }
+    
+    return 0;
+}
