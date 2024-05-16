976123cd3d17066f85a86ef8c0910d3b7d96a804
Suh-Jeong-Hyeon <150584831+CraftsManShip001@users.noreply.github.com>
2024-05-16 11:44:51 +0900

    [Gold V] Title: 소트, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/1083. 소트/소트.py
@@ -0,0 +1,23 @@
+n = int(input())
+arr = list(map(int,input().split()))
+s = int(input())
+i = 0
+while(1):
+    if(s == 0 or i == n):
+        break
+    if(s > n):
+        m = arr.index(max(arr[i:]))
+    else:
+        if(n - i >= n-(s + i+1)):
+            m = arr.index(max(arr[i:s + i + 1]))
+        else:
+            m = arr.index(max(arr[i:]))
+    if(m != i):
+        temp = arr[m-1]
+        arr[m-1] = arr[m]
+        arr[m] = temp
+        s -= 1
+    else:
+        i += 1
+for i in arr:
+    print(i,end = ' ')
