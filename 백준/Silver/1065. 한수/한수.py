e0b70cfc04b2f57e3cf0205d3dc42544093f184f
Suh-Jeong-Hyeon <150584831+CraftsManShip001@users.noreply.github.com>
2024-06-14 07:55:54 +0900

    [Silver IV] Title: 한수, Time: 112 ms, Memory: 123316 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,10 @@
+n = int(input())
+count = 0
+for i in range(1,n + 1):
+    num = str(i)
+    if len(num) == 3:
+        if int(num[0])-int(num[1]) == int(num[1])-int(num[2]):
+            count += 1
+    elif len(num) != 4:
+        count += 1
+print(count)
