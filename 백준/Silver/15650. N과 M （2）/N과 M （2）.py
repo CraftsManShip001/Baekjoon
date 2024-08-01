8ec57bdad831230e7ea9978edb28be24eda1957f
서정현 <150584831+CraftsManShip001@users.noreply.github.com>
2024-08-02 00:09:33 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,17 @@
+def back(arr,idx):
+    global res
+    if idx > m:
+        res.append(arr)
+        return 0
+    else:
+        if idx == 1:
+            num = 1
+        else:
+            num = arr[-1] + 1
+        for i in range(num,n+1):
+            back(arr + [i], idx + 1)
+n,m = map(int,input().split())
+res = []
+back([],1)
+for i in res:
+    print(*i)
