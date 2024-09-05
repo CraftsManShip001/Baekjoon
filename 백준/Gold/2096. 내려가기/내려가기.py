1e7d859e9ab4b7aa67266717e7b86ec705b73396
서정현 <150584831+CraftsManShip001@users.noreply.github.com>
2024-09-05 19:25:27 +0900

    [Gold V] Title: 내려가기, Time: 204 ms, Memory: 111432 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = list(map(int,input().split()))
+dp_max = arr
+dp_min = arr
+for _ in range(n-1):
+    arr = list(map(int,input().split()))
+    dp_max = [arr[0] + max(dp_max[0],dp_max[1]),arr[1] + max(dp_max), arr[2] + max(dp_max[1],dp_max[2])]
+    dp_min = [arr[0] + min(dp_min[0],dp_min[1]),arr[1] + min(dp_min), arr[2] + min(dp_min[1],dp_min[2])]
+print(max(dp_max),min(dp_min))
