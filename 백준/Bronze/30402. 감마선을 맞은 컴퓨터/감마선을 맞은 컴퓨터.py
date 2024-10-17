d86ffddd4b491bab4156981c48730671019fc02c
서정현 <150584831+CraftsManShip001@users.noreply.github.com>
2024-10-17 09:42:31 +0900

    [Bronze IV] Title: 감마선을 맞은 컴퓨터, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30402. 감마선을 맞은 컴퓨터/감마선을 맞은 컴퓨터.py
@@ -0,0 +1,11 @@
+for _ in range(15):
+    n = list(input())
+    if 'w' in n:
+        res = 'chunbae'
+        c = 0
+    elif 'b' in n:
+        res = 'nabi'
+        
+    elif 'g' in n:
+        res = 'yeongcheol'
+print(res)
