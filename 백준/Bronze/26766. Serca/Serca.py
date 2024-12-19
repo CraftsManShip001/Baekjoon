03e012084309e0a8e40323c44103ecb17743f627
서정현 <150584831+CraftsManShip001@users.noreply.github.com>
2024-12-19 13:36:08 +0900

    [Bronze V] Title: Serca, Time: 268 ms, Memory: 111636 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,12 @@
+user_input = int(input())
+
+for _ in range(user_input) :
+    print(" @@@   @@@")
+    print("@   @ @   @")
+    print("@    @    @")
+    print("@         @")
+    print(" @       @ ")
+    print("  @     @  ")
+    print("   @   @   ")
+    print("    @ @    ")
+    print("     @     ")
