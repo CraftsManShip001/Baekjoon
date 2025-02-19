e39d8847c2c77b79174385229ed9228135399136
서정현 <150584831+CraftsManShip001@users.noreply.github.com>
2025-02-19 19:31:37 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,11 +1,5 @@
-# select rest_info.rest_id, rest_name,food_type,favorites,address,round(avg(review_score),2) as score
-# from rest_info,rest_review
-# where address like '서울%'
-# group by rest_info.rest_id
-# order by avg(review_score) desc, favorites desc
-
 select rest_review.rest_id, rest_name,food_type,favorites,address,round(avg(rest_review.review_score),2) as SCORE
 from rest_review,rest_info
 where rest_review.rest_id = rest_info.rest_id and address like '서울%'
-group by rest_review.rest_id
+group by rest_review.rest_id, rest_name,food_type,favorites,address
 order by round(avg(rest_review.review_score),2) desc, favorites desc
