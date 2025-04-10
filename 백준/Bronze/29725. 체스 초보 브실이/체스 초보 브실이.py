2ee5cff0aa94bfca51568af0600e0509e286b4f0
정현 <150584831+CraftsManShip001@users.noreply.github.com>
2025-04-11 08:37:30 +0900

    [Bronze IV] Title: 체스 초보 브실이, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/29725. 체스 초보 브실이/체스 초보 브실이.py
@@ -0,0 +1,8 @@
+chess={'P':1, 'p':-1, 'N':3, 'n':-3, 'B':3, 'b':-3, 'R':5, 'r':-5, 'Q':9, 'q':-9}
+piece=[]
+for _ in range(8):
+    piece.extend(list(input()))
+score=0
+for idx,val in chess.items():
+    score+=piece.count(idx)*val
+print(score)
